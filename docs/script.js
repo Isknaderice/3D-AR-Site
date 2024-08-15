c0f7c409b246d6c84fc03d499142433f09509c85
Isknaderice <121503918+Isknaderice@users.noreply.github.com>
2024-08-15 19:05:49 +0500

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("3D AR Site is loaded and ready!");
-    // Здесь может быть код для работы с AR или другими интерактивными элементами
-});
+function changeModel(model) {
+    const viewer = document.getElementById('viewer');
+    viewer.setAttribute('src', `models/${model}`);
+}
